5fd1ef63772cf69eb74c8358d7af49ef571b904e
AknasKivin <aknaskivin19@gmail.com>
2025-03-12 14:24:11 +0530

    3rd java program

---
+++ Method/File1.java
@@ -0,0 +1,11 @@
+public class File1{
+    public void add(int num1,int num2){
+        int num3=num1+num2;
+        System.out.println(num3);
+    }
+    public static void main(String args[])
+    {
+        File1 obj=new File1();
+        obj.add(20,30);
+    }
+}
