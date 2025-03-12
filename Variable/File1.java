09b1b15ff96afe07bd89bb1968f2cf950ac8c1ae
AknasKivin <aknaskivin19@gmail.com>
2025-03-12 13:54:18 +0530

    My first java program

--- Variable/File1.java
+++ Variable/File1.java
@@ -1,10 +1,10 @@
-public class Demo{
+public class File1{
     public void add(int num1,int num2){
-        int num3=num1+num2;
+         int num3=num1+num2;
         System.out.println(num3);
     }
     public static void main(String args[]){
-        Demo obj=new Demo();
+        File1 obj=new File1();
         obj.add(10,20);
     }
 }
