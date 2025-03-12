d1c70482be4b45226fc27d7248115e4af60c0459
AknasKivin <aknaskivin19@gmail.com>
2025-03-12 14:17:32 +0530

    completed 2nd commend\

--- commends/File2.java
+++ commends/File2.java
@@ -1,4 +1,8 @@
-public class File2{
+public class File2{ 
+    /*
+    1)demo
+    2)using variable
+    */
     public static void main(String args[]){
     int num1=20;// this is int value
     System.out.println(num1);
