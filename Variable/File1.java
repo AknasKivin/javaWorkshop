1ffde4166c4f0e7ebcd1b6c48fe2157a8ada77f0
AknasKivin <aknaskivin19@gmail.com>
2025-03-12 14:04:26 +0530

    my program2

--- Variable/File1.java
+++ Variable/File1.java
@@ -1,10 +1,6 @@
 public class File1{
-    public void add(int num1,int num2){
-         int num3=num1+num2;
-        System.out.println(num3);
-    }
     public static void main(String args[]){
-        File1 obj=new File1();
-        obj.add(10,20);
-    }
+    int num1=20;
+    System.out.println(num1);
+}
 }
