a02a603efee4d0e0a33996ca9cbe567d73a14d51
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 10:49:27 +0530

    conditional statement program

---
+++ Conditional_statement/Demo1.java
@@ -0,0 +1,11 @@
+public class Demo1{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("it is true value");
+        }
+        if(false)
+        {
+            System.out.println("It is false");
+        }
+    }
+}
