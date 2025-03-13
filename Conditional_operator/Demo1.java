378cb9c16933c61036a9e18280bd1381291ff00f
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 10:31:16 +0530

    conditinal operator commited

---
+++ Conditional_operator/Demo1.java
@@ -0,0 +1,9 @@
+public class Demo1{
+    public static void main(String args[]){
+        System.out.println(10<2);
+        System.out.println(20>4);
+        System.out.println(30>=56);
+        System.out.println(40<=7);
+        System.out.println(19!=9);
+    }
+}
