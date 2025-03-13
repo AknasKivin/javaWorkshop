df13d5ad71ab8a09cf2d8923649d238aaadc5b20
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 11:11:50 +0530

    positive negative program

---
+++ Positive_negative/Demo1.java
@@ -0,0 +1,13 @@
+public class Demo1{
+public static void main(String args[]){
+    int num=-10;
+     if(num>=0){
+            System.out.println("The num is positive");
+        }
+        else
+        {
+            System.out.println("The num is negative");
+        }
+
+}
+}
