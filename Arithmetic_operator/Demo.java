190ab6149e5d492e2c9f47b69e8547dc8114e54e
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 10:23:02 +0530

    Arithmetic operation program

---
+++ Arithmetic_operator/Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+    public static void main(String args[]){
+        System.out.println(10+2);
+        System.out.println(20-2);
+        System.out.println(6*2);
+        System.out.println(15/3);
+        System.out.println(10%2);
+    }
+}
