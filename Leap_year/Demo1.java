bc0e3143e2c7aaa4160b3ab8ca07f800ab5df6d2
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 14:22:25 +0530

    completed

---
+++ Leap_year/Demo1.java
@@ -0,0 +1,12 @@
+public class Demo1{
+    public static void main(String args[]){
+        int i=2020;
+            if(i%4==0){
+        System.out.println("Leap year");
+            }
+        else{
+            System.out.println("not a leap year");
+        }
+        
+        }
+    }
