d6188a4d4907f2a452f118222b9e4ff9e3c9e546
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 10:58:19 +0530

    If else program

---
+++ If_else/Demo1.java
@@ -0,0 +1,11 @@
+public class Demo1{
+    public static void main(String args[]){
+        if(false){
+            System.out.println("it is true value");
+        }
+        else
+        {
+            System.out.println("It is false");
+        }
+    }
+}
