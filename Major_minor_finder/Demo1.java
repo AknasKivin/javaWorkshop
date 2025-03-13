c13a17a1a354d1957336ba7339f039ba187c8cac
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 11:06:51 +0530

    java program

---
+++ Major_minor_finder/Demo1.java
@@ -0,0 +1,12 @@
+public class Demo1{
+    public static void main(String args[]){
+        int age=19;
+        if(age>=18){
+            System.out.println("The person is Major");
+        }
+        else
+        {
+            System.out.println("The person is Minor");
+        }
+    }
+}
