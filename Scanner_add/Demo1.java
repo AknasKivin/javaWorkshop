e9065282a93d5548b71226bc29f95421769046be
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 15:18:31 +0530

    completed

---
+++ Scanner_add/Demo1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Demo1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("num1:");
+        int x=sc.nextInt();
+        System.out.print("num2:");
+        int y=sc.nextInt();
+        int z=x+y;
+        System.out.println("total:"+z);
+
+    }
+}
