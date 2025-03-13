9e11960562f49ee0c7090c6d1820fe28b3c5d58f
AknasKivin <aknaskivin19@gmail.com>
2025-03-13 11:42:34 +0530

    positive or negative program

--- Positive_negative/Demo1.java
+++ Positive_negative/Demo1.java
@@ -1,8 +1,11 @@
 public class Demo1{
 public static void main(String args[]){
     int num=-10;
-     if(num>=0){
+     if(num>0){
             System.out.println("The num is positive");
+        }
+        else if(num==0){
+            System.out.println("The num is neutral");
         }
         else
         {
