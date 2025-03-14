61441d77dc3c2f0d51d05a893fe0227e049e8f8c
AknasKivin <aknaskivin19@gmail.com>
2025-03-14 11:48:54 +0530

    Split Commited

---
+++ Split/File1.java
@@ -0,0 +1,7 @@
+public class File1{
+    public static void main(String args[]){
+        String name="Hello,World";
+        String names[]=name.split(",");
+        System.out.println(names[0]);
+    }
+}
